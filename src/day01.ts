a1b4e24f4558e6102eb53df651d84e0eaa6e2dc3
Jeff Goodman <jeff.goodman@author-it.com>
2022-12-02 09:07:11 +1300

    Day 1 refactor

--- src/day01.ts
+++ src/day01.ts
@@ -9,7 +9,6 @@ export function partB(): number {
     const elves = getElves();
     elves.sort((a, b) => b - a);
     const top3 = elves.slice(0, 3);
-
     return (top3.reduce((a, b) => a + b, 0));
 
 }
@@ -17,15 +16,12 @@ export function partB(): number {
 function getElves(): number[] {
     const input = fs.readFileSync('./src/day01input.txt', 'utf8');
 
-    const elvesInput = input.split('\n\n');
     const elves: number[] = [];
-
-    elvesInput.forEach(elfInput => {
-        const calories = elfInput.split('\n');
+    input.split('\n\n').map(e => {
         let total = 0;
-        calories.forEach(c => total += +c);
+        e.split('\n').forEach(c => total += +c);
         elves.push(total);
-    })
+    });
 
     return elves;
 
