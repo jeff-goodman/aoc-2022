f19351a0c9dcddb00610673b327dd2c06827d262
Jeff Goodman <jeff.goodman@author-it.com>
2022-12-06 10:21:59 +1300

    Day 4

--- src/day04.ts
+++ src/day04.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const pairs = getPairs()
+const pairs = getPairs();
 
 export function partA() {
     let i = 0;
@@ -17,15 +17,18 @@ export function partB() {
     let i = 0;
     pairs.forEach(pair => {
         if(
-            (+pair[0][0] >= +pair[1][0] && +pair[0][0] <= +pair[1][1]) ||
-            (+pair[0][1] >= +pair[1][0] && +pair[0][1] <= +pair[1][1]) ||
-            (+pair[1][0] >= +pair[0][0] && +pair[1][0] <= +pair[0][1]) ||
-            (+pair[1][1] >= +pair[0][0] && +pair[1][1] <= +pair[0][1])
+            inRange(pair[0][0], pair[1]) ||
+            inRange(pair[0][1], pair[1]) ||
+            inRange(pair[1][0], pair[0]) ||
+            inRange(pair[1][1], pair[0])
         ) i++;
     });
     return i;
 }
 
+function inRange(num: string, range: string[]) {
+    return +num >= +range[0] && +num <= +range[1];
+}
 
 function getPairs(): any[][] {
     const input = fs.readFileSync('./src/day04input.txt', 'utf8');
