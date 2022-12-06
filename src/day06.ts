14118d447d4867b6a29331d22cf24b8e977d189b
Jeff Goodman <jeff.goodman@author-it.com>
2022-12-07 07:42:24 +1300

    Day 6

--- src/day06.ts
+++ src/day06.ts
@@ -1,46 +1,35 @@
 import * as fs from 'fs';
-import { exit } from 'process';
 
 const input = getInput();
 
 export function partA() {
-    let marker = 0;
-    input.split('').reduce((prev, curr, i, arr) => {
-        if(prev.length < 3) {
-            return prev += curr;
-        }
-        const found = checkDuplicates(prev + curr);
-        if(found !== 0) {
-            return prev.slice(1) + curr;
-        } else {
-            marker = i+1;
-            arr.splice(0);
-        }
-        return prev;
-    });
-    return marker;
+    return getMarker(input, 4);
 }
 
 export function partB() {
-    let marker = 0;
-    input.split('').reduce((prev, curr, i, arr) => {
-        if(prev.length < 13) {
-            return prev += curr;
-        }
-        const found = checkDuplicates(prev + curr);
-        if(found !== 0) {
-            return prev.slice(1) + curr;
-        } else {
-            marker = i+1;
-            arr.splice(0);
-        }
-        return prev;
-    });
-    return marker;
+   return getMarker(input, 14);
 }
 
 function getInput() {
     return fs.readFileSync('./src/day06input.txt', 'utf8');
+}
+
+function getMarker(message: string, l: number): number {
+    let marker = 0;
+    message.split('').reduce((prev, curr, i, arr) => {
+        if(prev.length < (l-1)) {
+            return prev += curr;
+        }
+        const found = checkDuplicates(prev + curr);
+        if(found !== 0) {
+            return prev.slice(1) + curr;
+        } else {
+            marker = i+1;
+            arr.splice(0);
+        }
+        return prev;
+    });
+    return marker;
 }
 
 function checkDuplicates(s: string) {
