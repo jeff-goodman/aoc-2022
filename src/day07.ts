07c4a4cd36c9bdcf735baeca6c3cde4645c27e16
Jeff Goodman <jeff.goodman@author-it.com>
2022-12-11 14:05:47 +1300

    Day 07

--- src/day07.ts
+++ src/day07.ts
@@ -1,72 +1,67 @@
 import * as fs from 'fs';
 
-type TreeContent = {
-    [key: string]: File | Folder;
-};
+const input = getInput();
+const folders: {[key: string]: number} = {'/': 0};
+let currentFolder = '';
 
-type File = number;
-
-interface Folder {
-    [key: string]: TreeContent;
-}
-
-const commands = getInput();
-const tree: Folder = {};
-let currentFolder = 'root';
-tree[currentFolder] = {};
+//  higher than 1_293_692
 
 export function partA() {
-    let i = 0;
-
-    while(i < commands.length) {
-        const cmd = getCommand(commands[i]);
-        const output = commands[i];
-        i++;
-
-        if (cmd === undefined) {
-
-            if (output.startsWith('dir ')) {
-                tree[currentFolder][output.substring(4)] = {};
-                continue;
-            } else {
-                const [size, filename] = output.split(' ');
-                addFile(filename, +size);
-                tree[currentFolder][filename] = +size;
-                continue;
-            }
-
-        } else if (['cd /', 'ls'].includes(cmd)) {
-            continue;
-        } else if (cmd?.startsWith('cd ..')) {
-            currentFolder = currentFolder.substring(0, currentFolder.lastIndexOf('/'))
-            continue;
-        } else if(cmd?.startsWith('cd ')) {
-            currentFolder += `/${cmd.substring(3)}`;
-            continue;
+    buildList();
+    let total = 0;
+    Object.values(folders).forEach(value => {
+        if(value <= 100_000) {
+            total += value;
         }
-
-    }
-    console.log(tree);
+    });
+    return total;
 }
 
 export function partB() {
-
-
+    const totalSpace = 70_000_000;
+    const neededSpace = 30_000_000;
+    const usedSpace = folders['/'];
+    const unusedSpace = totalSpace - usedSpace;
+    const toDelete = neededSpace - unusedSpace;
+    const bigEnough = Object.values(folders).filter(val => val > toDelete);
+    return Math.min(...bigEnough);
 }
 
 function getInput() {
     return fs.readFileSync('./src/day07input.txt', 'utf8').split('\n');
 }
 
-function getCommand(s: string): string | undefined {
-    if(s.startsWith('$ ')) {
-        return s.substring(2);
-    }
-    return undefined;
-}
-
-function addFile(filename: string, size: number) {
-    const path = currentFolder.split('/');
-    tree[path]
-
+function buildList() {
+    input.forEach(cmd => {
+        if (cmd.startsWith('$ cd ..')) {
+            let newFolder: string;
+            if(currentFolder.lastIndexOf('/') === 0) {
+                newFolder = '/';
+            } else {
+                newFolder = currentFolder.substring(0, Math.max(0, currentFolder.lastIndexOf('/')));
+            }
+            currentFolder = newFolder;
+        } else if (cmd.startsWith('$ cd ')) {
+            const prevFolder = currentFolder;
+            if(currentFolder.length < 1 || currentFolder === '/') {
+                currentFolder += cmd.substring(5);
+             } else {
+                currentFolder += `/${cmd.substring(5)}`;
+             }
+        } else if (cmd.startsWith('$ ls')) {
+            // do nothing
+        } else if (cmd.startsWith('dir ')) {
+            const temp = currentFolder.length <= 1 ? currentFolder + cmd.substring(4) : currentFolder + `/${cmd.substring(4)}`;
+            if(!folders[temp]) {
+                folders[temp] = 0;
+            }
+        } else {
+            const [size, filename] = cmd.split(' ');
+            Object.entries(folders).forEach(([key, value]) => {
+                if(currentFolder.startsWith(key)) {
+                    folders[key] = value + Number(size);
+                }
+            })
+        }
+    })
 }
